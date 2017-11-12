957765b4e727f67f9b70d7c7402b40b99a8d399d
LAPTOP-LVF36ML7\Krzysztof Reniecki <polkom21@gmail.com>
2017-11-12 20:22:22 +0100

    Test child window widget in test app

--- FguiTestApp/main.cpp
+++ FguiTestApp/main.cpp
@@ -12,23 +12,27 @@ int main()
 	// Create label
 	Fgui::Label *label = new Fgui::Label("Test label", gui->GetFont());
 	label->SetPadding(sf::Vector2f(0, 0));
-	label->SetPosition(sf::Vector2f(10, 10));
+	label->SetPosition(sf::Vector2f(0, 45));
 	//label->SetAlignment(Fgui::HorizontalAlignment::Center, Fgui::VerticalAlignment::Center);
-	gui->AddElement(label);
+	//gui->AddElement(label);
 
 	// Create button
 	Fgui::Button *button = new Fgui::Button("Close window", gui->GetFont());
 	button->SetPadding(sf::Vector2f(50, 25));
 	//button->SetAlignment(Fgui::HorizontalAlignment::Center, Fgui::VerticalAlignment::Center);
-	button->SetPosition(sf::Vector2f(10, 34));
+	button->SetPosition(sf::Vector2f(0, 0));
 
 	button->Connect(Fgui::Signals::onClick, [&]() { window.close(); });
 	
-	gui->AddElement(button);
+	//gui->AddElement(button);
 
 	// Create ChildWindow
 	Fgui::ChildWindow *win = new Fgui::ChildWindow("Test window", gui->GetFont());
-	win->SetPosition(sf::Vector2f(100, 100));
+	win->SetPosition(sf::Vector2f(10, 100));
+	win->SetSize(sf::Vector2f(300, 420));
+	label->SetTextColor(sf::Color::Green);
+	win->AddElement(label, "test label");
+	win->AddElement(button, "closebutton");
 
 	gui->AddElement(win);
 
