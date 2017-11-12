36b6ff3dae08a73a945345a4f2fcd5c96e2b1df6
LAPTOP-LVF36ML7\Krzysztof Reniecki <polkom21@gmail.com>
2017-11-12 20:22:04 +0100

    Create child window widget

--- FGUI/Widgets/ChildWindow.h
+++ FGUI/Widgets/ChildWindow.h
@@ -8,10 +8,17 @@ namespace Fgui {
 	{
 	private:
 		sf::Text windowName;
+		sf::RenderTexture widgetRenderer;
+		sf::RectangleShape header, closeBox;
 
+		sf::Vector2f diffPos = sf::Vector2f(0, 0);
+
+		bool display = true;
+		
 		// Style
 		sf::Color backgroundColor = sf::Color::White;
-		sf::Color headerBackgroundColor = sf::Color(40, 40, 40);
+		sf::Color headerBackgroundColor = sf::Color(120, 120, 120);
+		sf::Color closeBoxBackgroundColor = sf::Color::Red;
 	public:
 		ChildWindow(const sf::String name, sf::Font & font);
 		~ChildWindow();
@@ -21,6 +28,7 @@ namespace Fgui {
 		virtual void Draw(sf::RenderWindow & window, sf::Vector2f parentPosition) override;
 		virtual void Draw(sf::RenderTexture & window, sf::Vector2f parentPosition) override;
 		virtual void Update() override;
+		virtual void HandleInput(sf::Event event);
 
 		void SetSize(sf::Vector2f size);
 		const sf::Vector2f GetSize();
